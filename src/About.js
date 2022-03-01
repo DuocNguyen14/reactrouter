33e99c8c7e70c456b2574b9d88b77686057ec699
DuocNguyen14 <duocandrewnguyen3@gmail.com>
2022-03-01 13:33:35 +0800

    initial commit

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <main className='About'>
+        <h2>About</h2>
+        <p style={{marginTop: "1rem"}}>
+          This is a react tutorial
+        </p>
+    </main>
+  )
+}
+
+export default About
